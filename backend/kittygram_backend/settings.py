940d712e509b9520d75c8065ec37a0f65b39810e
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-09-01 20:47:56 +0800

    01review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,9 +11,9 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY', '*')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
