ac0af529af88608d63e8310e142f8c92870c4add
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-09-01 21:51:45 +0800

    01review

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,9 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include
-from django.urls import path
 from rest_framework import routers
+from cats.views import AchievementViewSet, CatViewSet
+
 from cats.views import AchievementViewSet, CatViewSet
 
 router = routers.DefaultRouter()
