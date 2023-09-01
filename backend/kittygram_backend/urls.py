58ada610c433eb0896c06ee02878e892e9aa4afc
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-09-01 22:51:21 +0800

    02review

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,4 +1,4 @@
-from cats.views import AchievementViewSet, CatViewSet
+from ..cats import AchievementViewSet, CatViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
